8ace6c790a373fe4b1c685b1e0060299c6252f96
Jman4190 <mannelly.john@gmail.com>
2019-04-25 20:42:00 -0700

    removed try except from player tool

--- nba_functions.py
+++ nba_functions.py
@@ -1,51 +1,66 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# ## Custom Functions Used for Projection Model
+## Custom Functions Used for Projection Model
 
-# In[ ]:
+# import modules
+import numpy as np
+import pandas as pd
 
+cols_to_norm = ['pts',
+    'min',
+    'fgm',
+    'fga',
+    'fg3m',
+    'fg3a',
+    'ftm',
+    'fta',
+    'oreb',
+    'dreb',
+    'ast',
+    'stl',
+    'tov',
+    'blk']
+
+season_list = [
+    '1996-97',
+    '1997-98',
+    '1998-99',
+    '1999-00',
+    '2000-01',
+    '2001-02',
+    '2002-03',
+    '2003-04',
+    '2004-05',
+    '2005-06',
+    '2006-07',
+    '2007-08',
+    '2008-09',
+    '2009-10',
+    '2010-11',
+    '2011-12',
+    '2012-13',
+    '2013-14',
+    '2014-15',
+    '2015-16',
+    '2016-17',
+    '2017-18',
+    '2018-19']
 
 # function to normalize data
 def normalize(col):
     return (col - col.min()) / (col.max() - col.min())
 
-
-# In[ ]:
-
-
 # apply normalize function for each column
 def vorp(df):
-    cols_to_norm = ['pts',
-                'min',
-               'fgm',
-               'fga',
-               'fg3m',
-               'fg3a',
-               'ftm',
-               'fta',
-               'oreb',
-               'dreb',
-               'ast',
-               'stl',
-               'tov',
-               'blk']
     for col_name in cols_to_norm:
         df['{}_norm'.format(col_name)] = normalize(df[col_name])
     return df
-
-
-# In[ ]:
-
 
 # function to calculate distance between two points
 def calc_distance(u, v):
     dist = np.sqrt(np.sum((u - v)**2))
     return dist
-
-
-# In[ ]:
-
 
 # create a function to find the player and the next season
 def find_player(df, player_id, season):
@@ -54,19 +69,15 @@ def find_player(df, player_id, season):
         if season == row.season_id and player_id == row.player_id:
             return row
 
-
-# In[ ]:
-
-
 def player_comparison_tool(df, current_player_season, current_player_id):
+    if (((df['season_id'] == current_player_season) & (df['player_id'] == current_player_id)).any() == False):
+        print('Can\'t find player with id {0} and season {1}'.format(current_player_id, current_player_season))
+        return
+    # might need to return None if so
     for row in df.itertuples():
         if current_player_season == row.season_id and current_player_id == row.player_id:
             current_player_id = row.player_id
             break
-            
-    if (current_player_id == None):
-        print('Can\'t find player with id {0} and season {1}'.format(current_player_id, current_player_season))
-        return
 
     current_player_vector = np.array([
         (df.loc[(df['player_id'] == current_player_id) & (df['season_id'] == current_player_season), 'pts_norm']).item(),
@@ -145,17 +156,14 @@ def player_comparison_tool(df, current_player_season, current_player_id):
         sum_stat = 0
         sum_weight = 0
         for index, row in ranked_df.iloc[1:11].iterrows():
-            # skip over the row if it was 2016-17 season because we can't take the next
+            # skip over the row if it was 2017-18 season because we can't take the next
             if row.season_id == '2018-19':
                 continue
             # get the players next season
             weight = (1 / row.distance)
             next_season = season_list[(season_list.index(row.season_id) + 1)]
             # find the player row with the id and the next season
-            player_next_season = find_player(df, row.player_id, next_season)
-            
-            # it's not grabbing the column weight
-            
+            player_next_season = find_player(ranked_df, row.player_id, next_season)
             sum_stat += getattr(player_next_season, col) * weight
             sum_weight += weight
         projected_stats['player_id'] = current_player_id
